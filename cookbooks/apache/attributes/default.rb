1edc21ecf9a6fc7c7282b1e6323f1582bb8a318b
abhishek <root@localhost>
2017-03-29 20:49:54 +0000

    My latest problematic commit

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["abhishek2"] = { "site_title" => "Abhishek2s website 
 default["apache"]["sites"]["abhishek2b"] = { "site_title" => "abhishek2b website is coming soon!", "port" => 80, "domain" => "abhishek-s2b.mylabserver.com" }
 default["apache"]["sites"]["abhishek3"] = { "site_title" => "abhishek3 website", "port" => 80, "domain" => "abhishek-s3.mylabserver.com" }
 
+default["author"]["name"] = "abhishek"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
